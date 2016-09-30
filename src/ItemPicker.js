a8f700cedb5fadce76d58f43af218e79baf0a874
Erkan Sen <erkansen14@hotmail.com>
2016-09-30 13:51:35 -0500

    added side menu

---
+++ src/ItemPicker.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class ItemPicker extends Component {
+  render() {
+    return(
+      <div className='sideMenu'>
+      yolo swag 420
+      </div>
+    )
+  }
+}
