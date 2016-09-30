ceef63eaf1d48e6367bf432b6bb982106cdb0a7f
Erkan Sen <erkansen14@hotmail.com>
2016-09-30 14:06:15 -0500

    built burger building area

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import ItemPicker from './ItemPicker'
+import BuildArea from './BuildArea'
 import logo from './logo.svg';
 import './App.css';
 
+const 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <ItemPicker />
+      <BuildArea />
       </div>
     );
   }
---
+++ src/BuildArea.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class BuildArea extends Component {
+  render() {
+    return(
+      <div className='buildArea'>
+      
+      </div>
+    )
+  }
+}
