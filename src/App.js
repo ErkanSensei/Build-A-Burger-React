ea01c2362a02276937b26829a0019340161446c8
Erkan Sen <erkansen14@hotmail.com>
2016-10-02 21:26:15 -0500

    changed readme

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <ItemPicker />
-    <BuildArea condiments={condiments}/>
+        <ItemPicker />
+        <BuildArea condiments={condiments}/>
       </div>
     );
   }
